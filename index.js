c138838af4e6f961ddcdabf3c14ebc33c914b80b
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-04-13 22:25:08 -0500

    Added questions const in lieu of function, still writing

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const questions = [{
+    type: 'input',
+    name: 'employeeId',
+    message: "What's your employee ID?"
+},
+    {
+        type: 'input',
+        name: 'employeeEmail',
+        message: "What's your email address?"
+    },
+    {
+        type: 'list',
+        name: 'employeeRole',
+        message: 'Are you a manager, engineer, or intern?',
+        choices: ['Manager', 'Engineer', 'Intern']
+    }
+];
+
 function employeeQuestions() {
     inquirer
         .prompt(
