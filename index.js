30ef42f16a0d7a111b7045f87f9bb1cb5e5a2365
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-04-14 00:37:22 -0500

    Piecing together managerQuestions

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const fs = require('fs');
 
 const questions = [{
         type: 'input',
+        name: 'employeeName',
+        message: "What's your first and last name?"
+    },
+    {
+        type: 'input',
         name: 'employeeId',
         message: "What's your employee ID?"
-},
+    },
     {
         type: 'input',
         name: 'employeeEmail',
@@ -16,12 +21,64 @@ const questions = [{
         name: 'employeeRole',
         message: 'Are you a manager, engineer, or intern?',
         choices: ['Manager', 'Engineer', 'Intern']
-    }
+    },
 ];
 
 function employeeQuestions() {
     inquirer.prompt(questions).then(answers => {
+        const employeeName = `${answers.employeeName}`;
         const employeeRole = `${answers.employeeRole}`;
+        const employeeId = `${answers.EmployeeId}`;
+        const employeeEmail = `${answers.employeeEmail}`;
         })
 };
-employeeQuestions();
+function managerQuestions() {
+    console.log("Please build your team");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the team manager's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the team manager's id?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the team manager's email?",
+        validate: answer => {
+          const pass = answer.match(
+            /\S+@\S+\.\S+/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        }
+      }
+    ]).then(answers => {
+      teamQuestions()
+    });
+  }
+  
+managerQuestions();
+
+module.exports();
