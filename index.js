0018d69189d5801e410be04b34fe6acb52f6ffe3
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-04-13 20:01:27 -0500

    Working on employee questions

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const fs = require('fs');
 
 function employeeQuestions() {
     inquirer
-        .prompt({
+        .prompt(
+        {
             type: 'input',
             name: 'employeeId',
-            message: "What's your employee ID?",
+            message: "What's your employee ID?"
         },
         {
             type: 'input',
             name: 'employeeEmail',
-            message: "What's your email address?",
-        }
-        )
+            message: "What's your email address?"
+        })
         .then(() => {
+            inquirer
+                .prompt({
+                    type: 'list',
+                    name: 'employeeRole',
+                    message: 'Are you a manager, engineer, or intern?',
+                    choices: ['Manager', 'Engineer', 'Intern'],
+                });
             });
-        });
-}
+        };
+
+employeeQuestions();
