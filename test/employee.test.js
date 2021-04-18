fd248f195f35739ffcfbc930df5222c8d7afd4bb
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-04-18 00:00:44 -0500

    All tests are running and passing, edited classes and test files to ensure passing

--- test/employee.test.js
+++ test/employee.test.js
@@ -5,21 +5,21 @@ test("Can instantiate Employee instance", () => {
 });
 
 test("Can set name via constructor arguments", () => {
-  const name = "Alice";
-  const e = new Employee(name);
-  expect(e.name).toBe(name);
+  const employeeName = "Alice";
+  const e = new Employee(employeeName);
+  expect(e.employeeName).toBe(employeeName);
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
-  expect(e.id).toBe(testValue);
+  expect(e.employeeId).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
-  expect(e.email).toBe(testValue);
+  expect(e.employeeEmail).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
